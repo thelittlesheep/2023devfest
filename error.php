6d1fab4a89d35fd417fd90c8e8ae83412625e169
thelittlesheep <lshuangktw@gmail.com>
2023-11-17 16:24:31 +0800

    add error page

---
+++ error.php
@@ -0,0 +1,4 @@
+<?php
+header("HTTP/1.1 500 Internal Server Error");
+echo "<h1> Hello DevFest! This is an Error page</h1>";
+?>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 echo "<h1> Hello DevFest! </h1>";
+echo '<a href="./error.php">Error Page</a>';
 ?>
